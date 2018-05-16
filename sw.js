e23347a2d17c63a4794881908345dbe4a8ba54d1
Andrey <>
2018-05-16 17:43:38 +0300

    PWA start

--- sw.js
+++ sw.js
@@ -1,11 +1,48 @@
-'use strict';
+// 'use strict';
+//
+// importScripts('sw-toolbox.js');
+//
+// toolbox.precache(["index.html","style/style.css"]);
+//
+// toolbox.router.get('/images/*', toolbox.cacheFirst);
+//
+// toolbox.router.get('/*', toolbox.networkFirst, {
+//   networkTimeoutSeconds: 5
+// });
 
-importScripts('sw-toolbox.js');
 
-toolbox.precache(["index.html","style/style.css"]);
 
-toolbox.router.get('/images/*', toolbox.cacheFirst);
-
-toolbox.router.get('/*', toolbox.networkFirst, {
-  networkTimeoutSeconds: 5
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('v1').then(function(cache) {
+            return cache.addAll([
+                'index.html',
+                'style/style.css',
+            ]);
+        })
+    );
 });
+
+// self.addEventListener('fetch', function(event) {
+//     event.respondWith(caches.match(event.request).then(function(response) {
+//         // caches.match() always resolves
+//         // but in case of success response will have value
+//         if (response !== undefined) {
+//             return response;
+//         } else {
+//             return fetch(event.request).then(function (response) {
+//                 // response may be used only once
+//                 // we need to save clone to put one copy in cache
+//                 // and serve second one
+//                 let responseClone = response.clone();
+//
+//                 caches.open('v1').then(function (cache) {
+//                     cache.put(event.request, responseClone);
+//                 });
+//                 return response;
+//             }).catch(function () {
+//                 return caches.match('/images/*');
+//             });
+//         }
+//     }));
+// });
